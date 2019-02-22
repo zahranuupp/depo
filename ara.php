95e7515501e87fdb795aba633894d9dffa4e5863
zahranuupp <47617189+zahranuupp@users.noreply.github.com>
2019-02-22 16:57:06 +0300

    Update ara.php

--- ara.php
+++ ara.php
@@ -1,8 +1,7 @@
 
-<?php 
+
 <form action="https://twilter-post-source.000webhostapp.com/index.php" method="POST">
   First name: <input type="text" name="name"><br>
   Last name: <input type="text" name="surname"><br>
   <input type="submit" value="Submit">
 </form>
-?>
