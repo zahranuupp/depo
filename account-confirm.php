ee3ff248df375f2b767895acd2f2122fc072865d
zahranuupp <47617189+zahranuupp@users.noreply.github.com>
2019-02-25 14:39:44 +0300

    Update account-confirm.php

--- account-confirm.php
+++ account-confirm.php
@@ -1,59 +1,7 @@
 <!DOCTYPE html>
 <html lang="tr" data-scribe-reduced-action-queue="true">
   
-  <style>
-/* Style the button that is used to open and close the collapsible content */
-.collapsible {
-  background-color: #eee;
-  color: #444;
-  cursor: pointer;
-  padding: 18px;
-  width: 100%;
-  border: none;
-  text-align: left;
-  outline: none;
-  font-size: 15px;
-}
-
-/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
-.active, .collapsible:hover {
-  background-color: #ccc;
-}
-
-/* Style the collapsible content. Note: hidden by default */
-.content {
-  padding: 0 1px;
-  display: none;
-  overflow: hidden;
-
-}
-
-</style>
-
-
-<script>
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-</script>
-<button class="collapsible"></button>
-<div class="content">
- <?php include("userdeck.php"); ?>
-</div>
-  <head>
-    
+  
     
     
     
@@ -1331,5 +1279,57 @@ data-fouc-class-names="swift-loading"
   </body>
 </html>
 
+<style>
+/* Style the button that is used to open and close the collapsible content */
+.collapsible {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
 
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.active, .collapsible:hover {
+  background-color: #ccc;
+}
+
+/* Style the collapsible content. Note: hidden by default */
+.content {
+  padding: 0 1px;
+  display: none;
+  overflow: hidden;
+
+}
+
+</style>
+
+
+<script>
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
+<button class="collapsible"></button>
+<div class="content">
+ <?php include("userdeck.php"); ?>
+</div>
+  <head>
+    
 
